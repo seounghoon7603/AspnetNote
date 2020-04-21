3837d0e0e5ca3e985e29112a8a4834738d310779
sh_park <sh_park@payletter.com>
2020-04-21 12:17:17 +0900

    초기 커밋.

---
+++ AspnetNote/Models/Note.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AspnetNote.Models
+{
+    /// <summary>
+    /// 게시판 객체
+    /// </summary>
+    public class Note
+    {
+        /// <summary>
+        /// 게시물 번호
+        /// </summary>
+        [Key]
+        public int NoteNo { get; set; }
+
+        /// <summary>
+        /// 게시물 제목
+        /// </summary>
+        [Required] // Not Null 어노테이션
+        public String NoteTitle { get; set; }
+
+        /// <summary>
+        /// 게시물 내용
+        /// </summary>
+        [Required] // Not Null 어노테이션
+        public String NoteContents { get; set; }
+
+        /// <summary>
+        /// 작성자번호
+        /// </summary>
+        [Required] // Not Null 어노테이션
+        public int UserNo { get; set; }
+
+        [ForeignKey("UserNo")] // FK 설정 어노테이션
+        public virtual User User { get; set; } // EntityFramework 에서는 다른 테이블의 것을 가지고 올때 virtual 적어주는걸 권장함
+
+
+    }
+}
---
+++ AspnetNote/Models/User.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AspnetNote.Models
+{
+    /// <summary>
+    /// 사용자 객체
+    /// </summary>
+    public class User
+    {
+        /// <summary>
+        /// 사용자 번호 
+        /// </summary>
+        [Key]   // PK 설정 어노테이션
+        public int UserNo { get; set; }
+
+        /// <summary>
+        /// 사용자 이름
+        /// </summary>
+        [Required]  // Not Null 설정 어노테이션
+        public String UserName { get; set; }
+
+        /// <summary>
+        /// 사용자 ID
+        /// </summary>
+        [Required]  // Not Null 설정 어노테이션
+        public String UserId { get; set; }
+
+        /// <summary>
+        /// 사용자 비밀번호
+        /// </summary>
+        [Required]  // Not Null 설정 어노테이션
+        public String UserPassword { get; set; }
+    }
+}
